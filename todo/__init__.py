647e056167e98fd12565a7ee589382c24fc7d89d
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-03-10 16:00:36 +0200

    change init

--- todo/__init__.py
+++ todo/__init__.py
@@ -4,7 +4,6 @@ from . import db, user, index
 from flask import Flask
 
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
