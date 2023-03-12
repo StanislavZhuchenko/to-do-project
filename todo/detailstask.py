18ce78aad8270cade361032efa310295dbffb86b
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-03-12 15:46:37 +0200

    added feature to edit tasks

--- todo/detailstask.py
+++ todo/detailstask.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from todo.db import get_db
 
 bp = Blueprint('detailstask', __name__, url_prefix='/task')
@@ -11,3 +11,31 @@ def current_task(id):
         'SELECT * FROM list WHERE id = ?', (id, )
     ).fetchone()
     return render_template('tasks/detailstask.html', cur_task=cur_task)
+
+
+@bp.route('/<int:id>/update', methods=('POST', ))
+def update_task(id):
+    db = get_db()
+    error = None
+    # добавить метод Гет чтобы загружались обновленные данные
+    if request.method == 'POST':
+        priority = request.form['priority']
+        title = request.form['title']
+        due_date = request.form['due_date']
+        description = request.form['description']
+
+        if not title:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'UPDATE list SET title = ?, description = ?, due_date = ?, priority =? WHERE id = ?',
+                (title, description, due_date, priority, id)
+            )
+            db.commit()
+        return redirect(url_for('detailstask.current_task', id=id))
+
+    return redirect(url_for('detailstask.current_task', id=id))
+
--- todo/index.py
+++ todo/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 from todo.db import get_db
 
 bp = Blueprint('index', __name__)
@@ -8,6 +8,7 @@ bp = Blueprint('index', __name__)
 def index():
     db = get_db()
     user_id = session.get('user_id')
+    error = None
     if request.method == 'GET':
         tasks = db.execute(
             'SELECT * FROM list WHERE list.user_id = ? ORDER BY priority DESC', (user_id, )
@@ -17,14 +18,20 @@ def index():
         title = request.form['title']
         status = 'In progress'
         priority = request.form['priority']
-        db.execute(
-            'INSERT INTO list (title, user_id, status, priority) '
-            'VALUES (?, ?, ?, ?)', (title, user_id, status, priority)
-        )
-        db.commit()
-        return redirect(url_for('index.index'))
 
-    return render_template('base.html', tasks=tasks)
+        if not title:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'INSERT INTO list (title, user_id, status, priority) '
+                'VALUES (?, ?, ?, ?)', (title, user_id, status, priority)
+            )
+            db.commit()
+        return redirect(url_for('index.index'))
+    return render_template('tasks/listoftasks.html', tasks=tasks)
 
 
 @bp.route('/remove', methods=('POST', ))
